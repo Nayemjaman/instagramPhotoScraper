6b0ec89ba4e2887c94bfa41164b878ceae24df5e
Nayem Jaman Tusher <njaman26@gmail.com>
2022-03-11 19:22:03 +0600

    logged in

--- ImageDownloaderBot.py
+++ ImageDownloaderBot.py
@@ -1,7 +1,29 @@
 #import all the modules
+from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import time
+
+
+
+driver  = webdriver.Chrome('C:/Users/USER/Desktop/insrtagram_pic/chromedriver.exe')
+
+driver.get('https://www.instagram.com')
+
+
+#username
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+#password
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+#enter username and password
+username.clear()
+username.send_keys("python_is_everywhere")
+password.clear()
+password.send_keys("forgot@password")
+
+#login button and click it
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
